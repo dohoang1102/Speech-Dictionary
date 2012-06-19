3ffc698a134df4a97662ec1bba9ae55e1ec2ffa6
Brian Noah <brianjoshuanoah@gmail.com>
2012-06-19 14:02:51 -0700

    Initial Commit

---
+++ Speech Dictionary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Speech Dictionary
+//
+//  Created by Brian Noah on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Speech Dictionary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Speech Dictionary
+//
+//  Created by Brian Noah on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
