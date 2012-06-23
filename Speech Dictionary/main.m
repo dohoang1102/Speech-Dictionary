5e5a9e65bab0dbf51d6b218616bb8bc5ca61a245
Brian Noah <brianjoshuanoah@gmail.com>
2012-06-23 14:26:09 -0700

    new

--- Speech Dictionary/main.m
+++ Speech Dictionary/main.m
@@ -3,7 +3,7 @@
 //  Speech Dictionary
 //
 //  Created by Brian Noah on 6/19/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Wolfram Creative. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
